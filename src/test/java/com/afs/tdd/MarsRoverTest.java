511e74b5d5d906019acb404fdf3bee14350b7ca1
BennyYuen09 <Benny.Yuen@agileexlab.net>
2021-10-21 16:32:41 +0800

    [MarsRover][Benny] test: add test case for MarsRover turning to left from its initial state

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -32,4 +32,18 @@ class MarsRoverTest {
         //Then
         assertEquals(expected, actual);
     }
+
+    @Test
+    void should_turn_to_west_when_process_command_given_initial_rover_l_command() {
+        //given
+        MarsRover marsRover = new MarsRover(0, 0, "N");
+        String command = "L";
+        String expected = "0 0 W";
+
+        //when
+        String actual = marsRover.processCommand(command);
+
+        //Then
+        assertEquals(expected, actual);
+    }
 }
